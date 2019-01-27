48e49e715aab01612a5c2e9d3ad320e5733094de
Roman Nazarenko <me@jtalk.me>
2019-01-27 12:21:10 +0000

    Add sample bio text + BBCode support for safer markup.

---
+++ home2-ui/src/bbcode/tags.js
@@ -0,0 +1,60 @@
+import React from "react";
+import parser, {Tag} from "bbcode-to-react";
+
+
+class BrTag extends Tag {
+    static toReact() {
+        return <br/>
+    }
+}
+
+class PTag extends Tag {
+    toReact() {
+        return <p>{this.getComponents()}</p>
+    }
+}
+
+class AbbrTag extends Tag {
+    toReact() {
+        return <abbr title={this.params.title}>{this.getComponents()}</abbr>
+    }
+}
+
+class H1Tag extends Tag {
+    toReact() {
+        return <h1>{this.getComponents()}</h1>
+    }
+}
+
+class H2Tag extends Tag {
+    toReact() {
+        return <h2>{this.getComponents()}</h2>
+    }
+}
+
+class H3Tag extends Tag {
+    toReact() {
+        return <h3>{this.getComponents()}</h3>
+    }
+}
+
+class H4Tag extends Tag {
+    toReact() {
+        return <h4>{this.getComponents()}</h4>
+    }
+}
+
+class H5Tag extends Tag {
+    toReact() {
+        return <h5>{this.getComponents()}</h5>
+    }
+}
+
+parser.registerTag('br', BrTag);
+parser.registerTag('p', PTag);
+parser.registerTag('abbr', AbbrTag);
+parser.registerTag('h1', H1Tag);
+parser.registerTag('h2', H2Tag);
+parser.registerTag('h3', H3Tag);
+parser.registerTag('h4', H4Tag);
+parser.registerTag('h5', H5Tag);
--- home2-ui/src/home/home-grid.js
+++ home2-ui/src/home/home-grid.js
@@ -2,15 +2,34 @@ import React from "react";
 import {Grid} from "semantic-ui-react";
 import OwnerCard from "./owner-card";
 import LatestPosts from "./latest-posts";
+import "../bbcode/tags";
+import parser from "bbcode-to-react";
 
 class HomeGrid extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: "[h1]Header [abbr title=\"Lenghty explanation\"]LE[/abbr][/h1]" +
+                " [p]Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium," +
+                " totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae" +
+                " dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit," +
+                " sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam" +
+                " est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius" +
+                " modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima" +
+                " veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea" +
+                " commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil" +
+                " molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?[/p]"
+        };
+    }
 
     render() {
         return <Grid stackable centered>
             <Grid.Row>
                 {/*{ Something about footer-margin style was below }*/}
-                <Grid.Column width={10}>
-                    I am Vasya!!!
+                <Grid.Column width={10} as="main">
+                    {parser.toReact(this.state.text)}
                 </Grid.Column>
                 <Grid.Column width={4}>
                     <OwnerCard ownerPhotoUrl="/images/avatar.png"
