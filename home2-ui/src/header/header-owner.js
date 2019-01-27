f1ab6ba77ee604ac9a4557e71038d41bcb764b6e
Roman Nazarenko <me@jtalk.me>
2019-01-27 12:18:02 +0000

    Minor margin improvements for owner icon.

--- home2-ui/src/header/header-owner.js
+++ home2-ui/src/header/header-owner.js
@@ -5,7 +5,7 @@ class HeaderOwner extends React.Component {
 
     render() {
         return <Menu.Item>
-            <img src="/images/icon16.png" style={{height: '16px', width: '16px'}} alt="Icon"/>
+            <img src="/images/icon16.png" style={{height: '16px', width: '16px', marginRight: '10px'}} alt="Icon"/>
             <b>{this.props.ownerName}</b>
         </Menu.Item>
     }
