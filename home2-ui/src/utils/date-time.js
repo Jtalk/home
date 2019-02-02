b69f70a7a51961c59804acd532323fab0cd878d8
Roman Nazarenko <me@jtalk.me>
2019-02-02 20:12:36 +0000

    Improved date-time error reporting.

--- home2-ui/src/utils/date-time.js
+++ home2-ui/src/utils/date-time.js
@@ -1,9 +1,9 @@
-
+import * as what from "what.js"
 
 export function formatDateTime(date) {
     if (date instanceof Date) {
         return date.toLocaleString();
     } else {
-        throw Error("Unsupported date-time type: " + date);
+        throw Error("Unsupported date-time type: " + what(date));
     }
 }
