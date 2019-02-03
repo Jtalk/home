6cc461f869cdeedb404ff9e8a1b6927c2b40264d
Roman Nazarenko <me@jtalk.me>
2019-02-03 14:45:49 +0000

    Footer logo list adjustment (configurable spacing, style on a instead of img).

--- home2-ui/src/footer/flat-logo-list.js
+++ home2-ui/src/footer/flat-logo-list.js
@@ -5,11 +5,11 @@ export default class FlatLogoList extends React.Component {
     render() {
         return this.props.logos.map((logo, i) => {
             var style = {};
-            if (i > 0) {
-                style = {marginLeft: '30px !important'};
+            if (i > 0 && this.props.spacing) {
+                style = {marginLeft: this.props.spacing + ' !important'};
             }
-            return <a key={logo.name} href={logo.href}>
-                <img src={logo.src} height={logo.height} alt={logo.name} style={style}/>
+            return <a key={logo.name} href={logo.href} style={style}>
+                <img src={logo.src} height={logo.height} alt={logo.name}/>
             </a>
         });
     }
--- home2-ui/src/footer/footer.js
+++ home2-ui/src/footer/footer.js
@@ -6,10 +6,10 @@ import FlatLogoList from "./flat-logo-list";
 export default class Footer extends React.Component {
 
     render() {
-        return <Segment inverted basic className="footer" as="footer">
+        return <Segment inverted basic className="footer" as="footer" textAlign="center">
             <Container textAlign="center">
                 <Segment inverted basic textAlign="center">
-                    <FlatLinksList links={this.props.links} splitter="|"/>
+                    <FlatLinksList links={this.props.links} separator="|"/>
                 </Segment>
                 <FlatLogoList logos={this.props.logos}/>
             </Container>
