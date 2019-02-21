1d1645a514903a08cdd8a33ea33b2fab95f7bb6e
Roman Nazarenko <me@jtalk.me>
2019-02-21 21:41:06 +0000

    Skip sleeping entirely if api delay is not configured.

--- home2-ui/src/utils/test-api-delay.js
+++ home2-ui/src/utils/test-api-delay.js
@@ -2,5 +2,8 @@ import {sleep} from "sleepjs";
 import * as config from "react-global-configuration";
 
 export async function apiDelay() {
-    await sleep(config.get("apiDelay").milliseconds());
+    let delay = config.get("apiDelay");
+    if (delay) {
+        await sleep(delay.milliseconds());
+    }
 }
