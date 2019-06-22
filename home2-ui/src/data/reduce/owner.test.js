b0831c71d8b916293c983cb85df049279542fa1f
Roman Nazarenko <me@jtalk.me>
2019-06-22 21:41:12 +0100

    Default owner test (more to go)

---
+++ home2-ui/src/data/reduce/owner.test.js
@@ -0,0 +1,61 @@
+
+import * as config from "react-global-configuration";
+config.set({}, { freeze: false });
+
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import {reducers} from "../redux";
+import thunk from "redux-thunk";
+import * as request from "superagent";
+import * as owner from "./owner";
+
+const defaultOwner = {
+    name: "",
+    photoId: "",
+    nickname: "",
+    description: "",
+    contacts: [],
+    bio: ""
+};
+
+const mockOwner = {
+    name: "Vasya Pupkin",
+    photoId: "",
+    nickname: "pupkovas",
+    description: "A bloke",
+    contacts: [{contactType: "EMAIL", value: ""}],
+    bio: "Some lengthy bio"
+};
+
+describe('Redux(owner)', () => {
+    let store;
+    let sm;
+    beforeEach(() => {
+        store = createStore(
+            combineReducers({
+                ...reducers
+            }),
+            applyMiddleware(
+                thunk,
+            )
+        );
+        config.set({
+            api: '',
+        });
+        sm = require('superagent-mocker')(request);
+        sm.get('/owner', () => {
+            return { body: mockOwner };
+        });
+        sm.clearRoutes();
+    });
+    afterEach(() => {
+        sm && sm.clearRoutes();
+        config.set({});
+    });
+    test('default owner is provided before load', () => {
+        let ownerState = {a: 1};
+        store.subscribe(() => ownerState = store.getState().owner.toJS());
+        owner.load()(store.dispatch.bind(store));
+        expect(ownerState.data).toEqual(defaultOwner);
+        expect(ownerState.loading).toBeTruthy();
+    })
+});
