83d211dfb5c8ecb4f899ff8149ba2709dc99a3d7
Roman Nazarenko <me@jtalk.me>
2019-06-22 21:46:46 +0100

    Fix import

--- home2-ui/src/utils/superagent-api.js
+++ home2-ui/src/utils/superagent-api.js
@@ -1,5 +1,5 @@
 import * as config from "react-global-configuration";
-import * as prefix from "superagent-prefix";
+import prefix from "superagent-prefix";
 
 export default function api(request) {
     let apiPrefix = config.get("api");
