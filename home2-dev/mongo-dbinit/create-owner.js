650c2f3ac00c6eb367d5abf2ed774d6dfd0865cb
Roman Nazarenko <me@jtalk.me>
2020-01-12 13:24:15 +0000

    Owner now gets created with an avatar on dev env.

--- home2-dev/mongo-dbinit/create-owner.js
+++ home2-dev/mongo-dbinit/create-owner.js
@@ -1,17 +1,19 @@
 
-print('Creating collection Owner')
-db.createCollection('owner');
+print('Creating collection Owner');
+
+photoId = db['fs.files'].findOne({filename: 'avatar.png'})._id
+print('Using GridFS object as the owner\'s photo: ' + photoId);
+
 db.owner.insert({
 	name: 'Vasya Pupkin',
 	nickname: 'pupkivas',
 	description: 'A very cool guy with his own website',
-	photoId: '',
+	photoId: photoId,
 	bio: 'A very cool guy.\nHe has his very own website right here!',
 	contacts: [
 		{ contactType: 'EMAIL', value: 'pupkivas@example.com' },
 		{ contactType: 'SKYPE', value: 'pupkivas' },
 		{ contactType: 'TWITTER', value: '@pupkivas' }
 	]
-
 });
-print('Collection Owner created')
+print('Created collection Owner');
