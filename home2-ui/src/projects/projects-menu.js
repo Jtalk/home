3aa5bff74b609573770e602b020bc31d3b8fb4fd
Roman Nazarenko <me@jtalk.me>
2020-02-20 22:45:28 +0000

    Fixed project menu warning about react keys.

--- home2-ui/src/projects/projects-menu.js
+++ home2-ui/src/projects/projects-menu.js
@@ -6,14 +6,14 @@ import {Link} from "react-router-dom";
 export const ProjectsMenu = function ({projects, selectedProjectId}) {
 
     return <Menu tabular layout="block">
-        {projects.map(p => <ProjectTab project={p} selectedProjectId={selectedProjectId}/>)}
+        {projects.map(p => <ProjectTab key={p.id} project={p} selectedProjectId={selectedProjectId}/>)}
     </Menu>
 };
 
 export const ProjectTab = function ({project, selectedProjectId}) {
     if (project.id === selectedProjectId) {
-        return <div key={project.id} className="active item">{project.title}</div>
+        return <div className="active item">{project.title}</div>
     } else {
-        return <Link key={project.id} className="item" to={`/projects/${project.id}`}>{project.title}</Link>
+        return <Link className="item" to={`/projects/${project.id}`}>{project.title}</Link>
     }
 };
