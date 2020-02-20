8cbea97b5e94223e1fe7b5f4fdec84d2a1383565
Roman Nazarenko <me@jtalk.me>
2020-02-20 23:01:44 +0000

    Fix highlighting the first project's tab as active at /projects

--- home2-ui/src/projects/projects-menu.js
+++ home2-ui/src/projects/projects-menu.js
@@ -6,12 +6,12 @@ import {Link} from "react-router-dom";
 export const ProjectsMenu = function ({projects, selectedProjectId}) {
 
     return <Menu tabular layout="block">
-        {projects.map(p => <ProjectTab key={p.id} project={p} selectedProjectId={selectedProjectId}/>)}
+        {projects.map((p, i) => <ProjectTab key={p.id} project={p} active={(selectedProjectId && p.id === selectedProjectId) || i === 0}/>)}
     </Menu>
 };
 
-export const ProjectTab = function ({project, selectedProjectId}) {
-    if (project.id === selectedProjectId) {
+export const ProjectTab = function ({project, active}) {
+    if (active) {
         return <div className="active item">{project.title}</div>
     } else {
         return <Link className="item" to={`/projects/${project.id}`}>{project.title}</Link>
