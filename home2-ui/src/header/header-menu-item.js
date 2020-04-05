bb516218c7f3fa0c524f750121111c1e27cfe708
Roman Nazarenko <me@jtalk.me>
2020-04-05 13:47:17 +0100

    Always render menu items as links, even when active. It's much easier to navigate back this way.

--- home2-ui/src/header/header-menu-item.js
+++ home2-ui/src/header/header-menu-item.js
@@ -1,11 +1,6 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {Menu} from "semantic-ui-react";
 
 export const HeaderMenuItem = function ({title, href, active}) {
-    if (active) {
-        return <Menu.Item active>{title}</Menu.Item>
-    } else {
-        return <Link to={href} className="item">{title}</Link>
-    }
+    return <Link to={href} className={active ? "active item" : "item"}>{title}</Link>
 };
