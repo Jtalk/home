95bf037a9754bbbbb623252dcd43c3307d530d61
Roman Nazarenko <me@jtalk.me>
2020-04-07 20:33:42 +0100

    Provide a default site title until owner is loaded.

--- home2-ui/src/home/owner-titled.js
+++ home2-ui/src/home/owner-titled.js
@@ -4,7 +4,7 @@ import React from "react";
 
 export const OwnerTitled = function ({children}) {
     let {name} = useOwner();
-    return <Titled title={() => name}>
+    return <Titled title={() => name || "Home"}>
         {children}
     </Titled>
 };
