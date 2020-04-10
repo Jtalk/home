f09f4e1d2b31c23e85d0284cc45aa34c7c6cad25
Roman Nazarenko <me@jtalk.me>
2020-04-10 18:39:32 +0100

    It looks a tad better this way.

--- home2-ui/src/form/form-message.js
+++ home2-ui/src/form/form-message.js
@@ -3,7 +3,7 @@ import {Message} from "semantic-ui-react";
 
 export const ErrorMessage = function ({message}) {
     return <Message error>
-        <Message.Header>Error:</Message.Header>
+        <Message.Header>Error</Message.Header>
         {message}
     </Message>
 };
