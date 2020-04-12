475881746382ce7e9c4aa58560bfe92bb9599f4e
Roman Nazarenko <me@jtalk.me>
2020-04-12 17:33:09 +0100

    Set up initial login/password when initialising an empty server.

--- home2-rest/app/Module.scala
+++ home2-rest/app/Module.scala
@@ -1,9 +1,11 @@
 import java.time.Clock
 
 import com.google.inject.AbstractModule
+import jobs.AuthenticationInit
 
 class Module extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[Clock]).toInstance(Clock.systemUTC())
+    bind(classOf[AuthenticationInit]).asEagerSingleton()
   }
 }
