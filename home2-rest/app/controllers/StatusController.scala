0b5682ba24514f3a2ad2d61fd800036e9575c5db
Roman Nazarenko <me@jtalk.me>
2020-04-13 20:48:37 +0100

    Status controller in API.

---
+++ home2-rest/app/controllers/StatusController.scala
@@ -0,0 +1,30 @@
+package controllers
+
+import db.Database
+import javax.inject._
+import models.blog.Article
+import models.owner.OwnerInfo
+import play.api.Logger
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class StatusController @Inject()(cc: ControllerComponents, db: Database)
+  extends AbstractController(cc) {
+
+  private final val log = Logger(this.getClass)
+
+  implicit private def ec: ExecutionContext = cc.executionContext
+
+  def status() = Action.async { implicit request: Request[AnyContent] =>
+    db.findSingle[OwnerInfo]
+      .map(_ => Ok("UP"))
+      .recover {
+        case e =>
+          log.error("The database is unreachable", e)
+          ServiceUnavailable("DOWN")
+      }
+  }
+}
