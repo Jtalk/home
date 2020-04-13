37c616643a2130a43c27e57217a57d6171ec2236
Roman Nazarenko <me@jtalk.me>
2020-04-13 13:48:01 +0100

    Typo fix.

--- home2-ui/src/projects/project-description.js
+++ home2-ui/src/projects/project-description.js
@@ -7,7 +7,7 @@ export const ProjectDescription = function (project) {
 
     return <Grid stackable centered layout="block">
         <Grid.Column width={3}>
-            <OptionalImage size="small" id={project.logoId} alt={project.title + " Logo"}/>}
+            <OptionalImage size="small" id={project.logoId} alt={project.title + " Logo"}/>
             <Menu fluid vertical text layout="block">
                 <div className="active item">Overview</div>
                 {project.links && project.links.map(link => <a key={link.name} className="item" href={link.href}>{link.name}</a>)}
