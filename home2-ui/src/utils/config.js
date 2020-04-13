c6ea4ea69355402dafc7ba589d2f6a9da0c00322
Roman Nazarenko <me@jtalk.me>
2020-04-13 20:05:23 +0100

    Dockerfile for UI

--- home2-ui/src/utils/config.js
+++ home2-ui/src/utils/config.js
@@ -1,11 +1,15 @@
 import config from "react-global-configuration";
 import * as Duration from "duration-js";
 
-let API = "http://localhost:8090";
+let API = process.env["REACT_APP_API_URL"] || "http://localhost:8090";
+let DELAY = Duration.parse(process.env["REACT_APP_TEST_DURATION"] || "0s")
+
+console.info("API server is set to", API);
+console.info("API response delay is set to", DELAY);
 
 config.set({
     api: API,
-    apiDelay: Duration.parse("1s"),
+    apiDelay: DELAY,
     image: {
         url: {
             template: `${API}/images/{}`
