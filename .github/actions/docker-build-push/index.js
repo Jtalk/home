70f66875c35b977a716c678deacfa63e579e2ce7
Roman Nazarenko <me@jtalk.me>
2020-04-14 21:47:54 +0100

    A push-to-multiple-tags action.

---
+++ .github/actions/docker-build-push/index.js
@@ -0,0 +1,80 @@
+import {debug, getInput, getState, info, setFailed, setSecret} from "@actions/core";
+import {GitHub, context} from "@actions/github";
+import {execSync} from "child_process";
+
+try {
+    let {sha, ref} = context;
+    if (ref && ref.startsWith("refs/heads/")) {
+        ref = ref.substring(0, "refs/heads/".length);
+    }
+    if (ref && ref.startsWith("refs/tags/")) {
+        ref = ref.substring(0, "refs/tags/".length);
+    }
+    let isMaster = ref === "master";
+    let tagPrefix = getInput("tag-prefix", {required: true});
+    let dockerfile = getInput("dockerfile");
+    let registry = getInput("registry");
+    let username = getInput("username");
+    let password = getInput("password");
+    setSecret(password);
+
+    let tag = withRegistry(registry, `${tagPrefix}:${sha}`);
+    let refTag = ref && withRegistry(registry, `${tagPrefix}:${refTag}`);
+    let latestTag = withRegistry(registry, `${tagPrefix}:latest`);
+
+    info("Logging into the registry " + registry);
+    docker.login(registry, username, password);
+    info(`Building from ${dockerfile} to ${tag}`);
+    docker.build(dockerfile, tag);
+    info(`Pushing tag ${tag}`);
+    docker.push(tag);
+    if (ref) {
+        docker.tag(tag, refTag);
+        info(`Ref '${ref}' build, pushing tag ${refTag}`);
+        docker.push(refTag);
+    }
+    if (isMaster) {
+        docker.tag(tag, latestTag);
+        info(`Master branch build, pushing tag ${latestTag}`);
+        docker.push(latestTag);
+    }
+    info("Done");
+} catch (e) {
+    console.error("Error running action docker-build-push", e);
+    setFailed(e.message);
+}
+
+function withRegistry(registry, tag) {
+    if (registry) {
+        return `${registry}/${tag}`;
+    } else {
+        return tag;
+    }
+}
+
+const docker = {
+    processConfig: {
+        stdio: "inherit",
+        encoding: "utf8",
+    },
+    login: (registry = "", username, password) => {
+        let output = execSync(`docker login -u ${username} --password-stdin ${registry}`, {
+            input: password,
+            ...this.processConfig
+        });
+        debug(output);
+    },
+    build: (dockerfile, tag) => {
+        let dockerfileOpt = dockerfile && `-f '${dockerfile}'`;
+        let output = execSync(`docker build ${dockerfileOpt} -t '${tag}' .`, this.processConfig);
+        debug(output);
+    },
+    tag: (from, to) => {
+        let output = execSync(`docker tag '${from}' '${to}'`, this.processConfig);
+        debug(output);
+    },
+    push: (tag) => {
+        let output = execSync(`docker push '${tag}'`, this.processConfig);
+        debug(output);
+    },
+}
