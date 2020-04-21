25af3fed80157d861eb1909bb156ea9d9bda914f
Roman Nazarenko <me@jtalk.me>
2020-04-21 23:43:34 +0100

    Printing a success message at the end of the backup process

--- home2-dbbackup/entrypoint.sh
+++ home2-dbbackup/entrypoint.sh
@@ -7,3 +7,5 @@ aws configure set aws_access_key_id $AWS_KEY_ID
 aws configure set aws_secret_access_key $AWS_KEY
 BACKUP_TIMESTAMP=$(date +%s)
 find "$BACKUP_NAME" -type f -exec aws s3 cp {} "s3://$BUCKET_PATH/$BACKUP_NAME/$BACKUP_TIMESTAMP/" --storage-class=DEEP_ARCHIVE \;
+
+echo Success
