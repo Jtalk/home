e8d2c35364c0617a5ca72deee9cabdd34b47d7ab
Roman Nazarenko <me@jtalk.me>
2020-04-21 23:26:01 +0100

    Creating a backup cronjob for the DB

---
+++ home2-dbbackup/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+BACKUP_NAME=backup-$(date +"%Y-%m-%d")
+mongodump --uri="$MONGO_URL" -o "$BACKUP_NAME"
+
+aws configure set aws_access_key_id $AWS_KEY_ID
+aws configure set aws_secret_access_key $AWS_KEY
+BACKUP_TIMESTAMP=$(date +%s)
+find "$BACKUP_NAME" -type f -exec aws s3 cp {} "s3://$BUCKET_PATH/$BACKUP_NAME/$BACKUP_TIMESTAMP/" --storage-class=DEEP_ARCHIVE \;
