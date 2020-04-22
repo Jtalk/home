61371a04e1cdbb479a2293f45ba38f785a65a36a
Roman Nazarenko <me@jtalk.me>
2020-04-22 23:55:41 +0100

    Fix index creation syntax. Apparently, my intellij was picking some very wrong source up for its autocomplete...

--- home2-dbmigrate/migrations/20200422174108-indices.js
+++ home2-dbmigrate/migrations/20200422174108-indices.js
@@ -2,26 +2,26 @@ module.exports = {
   async up(db, client) {
 
     console.log("Creating unique indices");
-    await db.collection("session").createIndex("session.id", "id", {unique: true});
-    await db.collection("articles").createIndex("articles.id", "id", {unique: true});
-    await db.collection("projects").createIndex("projects.id", "id", {unique: true});
-    await db.collection("authentication").createIndex("authentication.id", "id", {unique: true});
-    await db.collection("authentication").createIndex("authentication.login", "login.login", {unique: true});
+    await db.collection("session").createIndex("id", {unique: true, name: "session.id"});
+    await db.collection("articles").createIndex("id", {unique: true, name: "articles.id"});
+    await db.collection("projects").createIndex("id", {unique: true, name: "projects.id"});
+    await db.collection("authentication").createIndex("id", {unique: true, name: "authentication.id"});
+    await db.collection("authentication").createIndex("login.login", {unique: true, name: "authentication.login"});
 
     console.log("Creating sort indices");
-    await db.collection("articles").createIndex("articles.published-created", ["published", "created"]);
-    await db.collection("projects").createIndex("projects.published-order", ["published", "order"]);
+    await db.collection("articles").createIndex(["published", "created"], {name: "articles.published-created"});
+    await db.collection("projects").createIndex(["published", "order"], {name: "projects.published-order"});
   },
 
   async down(db, client) {
 
-    await db.collection("session").deleteIndex("session.id");
-    await db.collection("articles").deleteIndex("articles.id");
-    await db.collection("projects").deleteIndex("projects.id");
-    await db.collection("authentication").deleteIndex("authentication.id");
-    await db.collection("authentication").deleteIndex("authentication.login");
+    await db.collection("session").dropIndex("session.id");
+    await db.collection("articles").dropIndex("articles.id");
+    await db.collection("projects").dropIndex("projects.id");
+    await db.collection("authentication").dropIndex("authentication.id");
+    await db.collection("authentication").dropIndex("authentication.login");
 
-    await db.collection("articles").deleteIndex("articles.published-created");
-    await db.collection("projects").deleteIndex("projects.published-order");
+    await db.collection("articles").dropIndex("articles.published-created");
+    await db.collection("projects").dropIndex("projects.published-order");
   }
 };
