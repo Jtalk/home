b4081762f7f0d4883ef21bc0166e6eeb8132fa56
Roman Nazarenko <me@jtalk.me>
2020-04-24 20:45:16 +0100

    Add neat spacing around the footer logos

--- home2-ui/src/footer/footer.js
+++ home2-ui/src/footer/footer.js
@@ -3,6 +3,7 @@ import {Container, Segment} from "semantic-ui-react";
 import FlatLinksList from "./flat-links-list";
 import {FlatLogoList} from "./flat-logo-list";
 import {useFooter} from "../data/reduce/footer";
+import "./footer.css";
 
 export const Footer = function () {
     let {links, logos} = useFooter();
@@ -13,10 +14,8 @@ export const StatelessFooter = function (props) {
 
     return <Segment inverted basic className="footer" as="footer" textAlign="center">
         <Container textAlign="center">
-            <Segment inverted basic textAlign="center">
-                <FlatLinksList links={props.links} separator="|"/>
-            </Segment>
-            <FlatLogoList logos={props.logos}/>
+            <FlatLinksList links={props.links} separator="|"/>
+            <FlatLogoList className="logos" logos={props.logos}/>
             <p>Copyright (C) 2020 Roman Nazarenko </p>
         </Container>
     </Segment>
