e0ad289e0263e106434a58e7030d1e745326130d
Roman Nazarenko <me@jtalk.me>
2020-04-24 20:45:34 +0100

    Readability improvements for flat-logo-list

--- home2-ui/src/footer/flat-logo-list.js
+++ home2-ui/src/footer/flat-logo-list.js
@@ -3,10 +3,14 @@ import "./flat-logo-list.css";
 
 const LOGO_HEIGHT = '40px';
 
-export const FlatLogoList = function ({logos}) {
-    return logos.map((logo, i) => {
-        return <a key={logo.name} className="footer logo" href={logo.href}>
-            <img src={logo.src} height={LOGO_HEIGHT} alt={logo.name}/>
-        </a>
-    });
+export const FlatLogoList = function ({logos, className}) {
+    return <div className={className}>
+        {logos.map(logo => <Logo {...logo}/>)}
+    </div>
+}
+
+export const Logo = function (logo) {
+    return <a key={logo.name} className="logo" href={logo.href}>
+        <img src={logo.src} height={LOGO_HEIGHT} alt={logo.name}/>
+    </a>
 }
