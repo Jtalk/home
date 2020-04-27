7212eb6b5c851fdd3816cb7a159500a3b141c781
Roman Nazarenko <me@jtalk.me>
2020-04-27 20:49:32 +0100

    Add extra bugsnag config

--- home2-ui/src/utils/error-reporting.js
+++ home2-ui/src/utils/error-reporting.js
@@ -2,11 +2,16 @@ import Bugsnag from "@bugsnag/js";
 import BugsnagPluginReact from "@bugsnag/plugin-react";
 import React from "react";
 
+const APP_VERSION = process.env.REACT_APP_VERSION;
+
 export function setupErrorReporting() {
     Bugsnag.start({
         apiKey: '899f8865a22e37b078415c92f56dfc5f',
         plugins: [new BugsnagPluginReact(React)],
-        releaseStage: "production"
+        appVersion: APP_VERSION,
+        enabledReleaseStages: ["production", "staging"],
+        maxBreadcrumbs: 50,
+        maxEvents: 20,
     })
     const ErrorBoundary = Bugsnag.getPlugin('react')
     return {ErrorBoundary};
