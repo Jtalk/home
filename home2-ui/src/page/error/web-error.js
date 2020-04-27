548a7391899f4426ec67e4fa58c19c9d8389e69f
Roman Nazarenko <me@jtalk.me>
2020-04-27 21:21:16 +0100

    Reporting an error page to bugsnag

--- home2-ui/src/page/error/web-error.js
+++ home2-ui/src/page/error/web-error.js
@@ -1,9 +1,11 @@
 import React from "react";
 import {Container} from "semantic-ui-react";
 import {Titled} from "react-titled";
+import {reportError} from "../../utils/error-reporting";
 
 export const WebError = function ({httpCode, message}) {
     console.error("Showing error page", httpCode, message);
+    reportError({errorClass: "error-page", errorMessage: `Showing error page: ${httpCode} ${message}`});
     return <Titled title={() => httpCode}>
         <Container text textAlign="center">
             {httpCode}: {message}
