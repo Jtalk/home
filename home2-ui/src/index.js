4273fdb7731910af09a8d5a0082f35de9d34534e
Roman Nazarenko <me@jtalk.me>
2020-04-28 23:01:18 +0100

    Removing unnecessary serviceWorker It causes errors in some firefoxes.

--- home2-ui/src/index.js
+++ home2-ui/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.css';
 import {App} from './App';
-import * as serviceWorker from './serviceWorker';
 import {createAppStore} from "./data/redux";
 import {Provider as ReduxProvider} from "react-redux";
 import {FileConverterProvider} from "./utils/file-converter-context";
@@ -27,7 +26,3 @@ ReactDOM.render(
     </ErrorBoundary>,
     document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
