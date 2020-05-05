b9896de4503321c8ffc27b9fb53acb15488167d6
Roman Nazarenko <me@jtalk.me>
2020-05-06 00:16:56 +0100

    Dev DB init updated

--- home2-dev/mongo-dbinit/create-blog.js
+++ home2-dev/mongo-dbinit/create-blog.js
@@ -9,6 +9,13 @@ function asIso(prefix, suffix, hour) {
 	return prefix + hourString + suffix;
 }
 
+function pad2(value, len) {
+	if (value.length === 1) {
+		return '0' + value;
+	}
+	return value;
+}
+
 db.articles.createIndex({id: 1}, {unique: true});
 db.articles.createIndex({
 	title: "text",
@@ -26,16 +33,20 @@ db.articles.insert({
 	id: 'test-article',
 	published: true,
 	created: '2020-01-20T10:00:01Z',
+	updated: "2020-05-01T13:00:00Z",
 	content: '# A very cool test article\n<preview>Once upon a time...</preview>\nOther content',
-	tags: ["Java", "Scala"]
+	tags: ["Java", "Scala"],
+	atomId: "b93f90f8-d3ca-480b-a71f-6ea3fda35004"
 });
 db.articles.insert({
 	title: 'Test Article 2',
 	id: 'another-test-article',
 	published: false,
 	created: '2020-01-22T14:00:01Z',
+	updated: "2020-05-01T14:00:00Z",
 	content: 'Another very cool test article',
-	tags: []
+	tags: [],
+	atomId: "8f855752-23a6-4af5-b434-31b9aeda4b6c"
 });
 Array(20).fill().forEach((_, i) => {
 	db.articles.insert({
@@ -43,8 +54,10 @@ Array(20).fill().forEach((_, i) => {
 		id: 'test-article-' + (i + 3),
 		published: true,
 		created: asIso('2020-01-20T', ':00:01Z', (i + 1)),
+		updated: asIso('2020-05-02T', ':00:01Z', (i + 1)),
 		content: '<preview>A very cool test article</preview>',
-		tags: ["Java", "Scala"]
+		tags: ["Java", "Scala"],
+	atomId: "c33f90f8-d3ca-480b-a71f-6ea3fda350" + pad2(i + 1)
 	});
 });
 db.articles.insert({
@@ -52,7 +65,9 @@ db.articles.insert({
 	id: 'last-test-article',
 	published: false,
 	created: '2020-01-22T14:00:01Z',
+	updated: "2020-05-01T12:00:00Z",
 	content: '# Another very cool test article\n<preview>Preview going ahead...</preview>\nSomething something',
-	tags: []
+	tags: [],
+	atomId: "fe72f0f5-9c71-445b-a9e9-ddc133958130"
 });
 print('Created collection Articles');
--- home2-dev/mongo-dbinit/create-owner.js
+++ home2-dev/mongo-dbinit/create-owner.js
@@ -21,11 +21,13 @@ db.owner.insert({
 	nickname: 'pupkivas',
 	description: 'A very cool guy with his own website',
 	photoId: photoId,
+	updated: "2020-05-01T12:00:00Z",
 	bio: '# A very cool guy.\n\nHe has his very own ~~website~~ right here?\n\n<InfoMessage header={Cool header}>Some big text</InfoMessage>\n\nMore description etc.',
 	contacts: {
 		email: { contactType: 'email', value: 'pupkivas@example.com' },
 		skype: { contactType: 'skype', value: 'pupkivas' },
 		twitter: { contactType: 'twitter', value: '@pupkivas' }
-	}
+	},
+	atomId: "3a602278-d1f7-4c17-be73-0985d5be979b"
 });
 print('Created collection Owner');
