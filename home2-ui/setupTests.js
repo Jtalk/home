62f51681ccc61b0a483c8decc5a2541743c66057
Roman Nazarenko <me@jtalk.me>
2020-10-17 19:45:05 +0100

    Jest setup

--- home2-ui/setupTests.js
+++ home2-ui/setupTests.js
@@ -1,4 +1,7 @@
 import * as Enzyme from "enzyme/build";
 import Adapter from "enzyme-adapter-react-16/build";
+import getConfig from "next/config";
 
 Enzyme.configure({adapter: new Adapter()});
+jest.mock("next/config");
+getConfig.mockImplementation(() => ({}));
