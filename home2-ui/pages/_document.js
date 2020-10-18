aa8196aeb275d551a54338acdb2d87f6dcffcfd2
Roman Nazarenko <me@jtalk.me>
2020-10-18 20:27:50 +0100

    Custom document

---
+++ home2-ui/pages/_document.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Document, {Main, NextScript, Html, Head} from "next/document";
+
+export default class MyDocument extends Document {
+    render() {
+        return <Html lang="en">
+            <Head>
+                <meta charSet="utf-8"/>
+                <link rel="shortcut icon" href="/images/icon32.png"/>
+                <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="/api/atom.xml"/>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+                <meta name="theme-color" content="#000000"/>
+                <link rel="manifest" href="/manifest.json"/>
+                <title>Homepage</title>
+            </Head>
+            <body className="pushable">
+                <noscript>You need to enable JavaScript to run this app.</noscript>
+                <div className="pusher">
+                    <Main/>
+                </div>
+                <NextScript/>
+            </body>
+        </Html>
+    }
+}
