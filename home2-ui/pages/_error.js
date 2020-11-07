0eb173a0945f64a97945f64509fad961084cb14d
Roman Nazarenko <me@jtalk.me>
2020-11-07 13:37:35 +0000

    Remove foul language from the error page

--- home2-ui/pages/_error.js
+++ home2-ui/pages/_error.js
@@ -2,15 +2,12 @@ import {WebError} from "../component/error/web-error";
 import React from "react";
 
 const Error = ({code, message}) => {
-  console.error(`wtf2`, code, message);
   return <WebError httpCode={code} message={message}/>
 }
 
 Error.getInitialProps = ({res, err}) => {
   const code = res ? res.statusCode : err ? err.statusCode : 404;
-  const result = { code, message: code === 404 ? "Not Found" : "Internal Error" }
-  console.error(`wtf`, result);
-  return result;
+  return {code, message: code === 404 ? "Not Found" : "Internal Error"};
 }
 
 export default Error;
