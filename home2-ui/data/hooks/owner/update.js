d50468e6aeec4edd9ac9c6bc0ef50e652ef67d46
Roman Nazarenko <me@jtalk.me>
2020-11-14 17:07:00 +0000

    Fix bio editing

--- home2-ui/data/hooks/owner/update.js
+++ home2-ui/data/hooks/owner/update.js
@@ -1,6 +1,20 @@
 import useUpdater from "../global/swr-common/updater";
 import { ownerApiUrl } from "./get";
+import { useCallback } from "react";
+import { useImageUploader } from "../images";
 
 export function useOwnerUpdater() {
-  return useUpdater(ownerApiUrl);
+  const { updater: nestedUpdater, error, status } = useUpdater(ownerApiUrl);
+  const { uploader: imageUploader } = useImageUploader(0);
+  const updater = useCallback(
+    async (data, { photo }) => {
+      if (photo) {
+        const uploaded = await imageUploader("owner-photo", photo);
+        data.photoId = uploaded.id;
+      }
+      return nestedUpdater(data);
+    },
+    [imageUploader, nestedUpdater]
+  );
+  return { updater, error, status };
 }
