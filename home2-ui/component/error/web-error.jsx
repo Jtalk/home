c167aa11f150334f7dce70bf42bdb20f3457030f
Roman Nazarenko <me@jtalk.me>
2020-11-14 18:46:45 +0000

    Show path when printing web error

--- home2-ui/component/error/web-error.jsx
+++ home2-ui/component/error/web-error.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { reportError } from "../../utils/error-reporting";
 import Head from "next/head";
 import Container from "semantic-ui-react/dist/commonjs/elements/Container";
+import { useRouter } from "next/router";
 
 export const WebError = function ({ httpCode, message }) {
-  console.error("Showing error page", httpCode, message);
-  reportError({ errorClass: "error-page", errorMessage: `Showing error page: ${httpCode} ${message}` });
+  const router = useRouter();
+  console.error("Showing error page at", router.asPath || router.pathname, httpCode, message);
+  reportError({
+    errorClass: "error-page",
+    errorMessage: `Showing error page at ${router.asPath || router.pathname}: ${httpCode} ${message}`,
+  });
   return (
     <Container text textAlign="center">
       <Head>
