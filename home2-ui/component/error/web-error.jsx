26d47dab9dda25b42c6154e9d7b28e4b91f203bd
Roman Nazarenko <me@jtalk.me>
2020-11-15 18:45:06 +0000

    Stop warnings about missing asset-manifest.json

--- home2-ui/component/error/web-error.jsx
+++ home2-ui/component/error/web-error.jsx
@@ -24,7 +24,7 @@ export const WebError = function ({ httpCode, message }) {
   );
 };
 
-const ignore404 = [/\.php(\?|$)/];
+const ignore404 = [/\.php(\?|$)/, /asset-manifest.json$/];
 function is404Ignored(path) {
   for (const ignored of ignore404) {
     if (ignored.exec(path)) return true;
