4c9041b125cf1fccd48982931d008ddd18a6169d
Roman Nazarenko <me@jtalk.me>
2021-06-26 10:39:56 +0100

    More ignores!

--- home2-ui/component/error/web-error.jsx
+++ home2-ui/component/error/web-error.jsx
@@ -25,11 +25,13 @@ export const WebError = function ({ httpCode, message }) {
 };
 
 const ignore404 = [
-  /\.php(\?|$)/,
-  /asset-manifest.json$/,
-  /\/(\.env|feed|ads\.txt|wp-content|rss\.xml|console|admin(istrator)?|vendor)/,
-  /\/index\.(rss|rdf)/,
-  /\/\.well-known\/acme-challenge\//,
+  /\.php(\?|$)/i,
+  /asset-manifest.json$/i,
+  /\/(\.env|feed|ads\.txt|wp-\w+|wp\d|rss\.xml|console|admin(istrator)?|vendor|bak|new-site|v\d|temp|\d{4}|dev|web|old(-\w+)?|cms|demo|backup)/i,
+  /\/(site|test|main|new|wordpress|index)/i,
+  /\/(\.git|DesktopModules|providers|Install|app_master|\.well-known|stalker_portal|c|home\/resources)/i,
+  /\/index\.(rss|rdf)/i,
+  /\/\.well-known\/acme-challenge\//i,
 ];
 function is404Ignored(path) {
   for (const ignored of ignore404) {
