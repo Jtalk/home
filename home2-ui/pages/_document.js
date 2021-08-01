a4762bc4acf5db90aea1220936f94b0edbce442c
Roman Nazarenko <me@jtalk.me>
2021-08-01 12:56:09 +0100

    This was a mistake

--- home2-ui/pages/_document.js
+++ home2-ui/pages/_document.js
@@ -7,10 +7,10 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="shortcut icon" href="images/icon32.png" />
-          <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="api/atom.xml" />
+          <link rel="shortcut icon" href="/images/icon32.png" />
+          <link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="/api/atom.xml" />
           <meta name="theme-color" content="#000000" />
-          <link rel="manifest" href="manifest.json" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body className="pushable">
           <div className="pusher">
