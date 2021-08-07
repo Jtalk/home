c63d13cc4a15ca654e0a4b10917c39ce05435210
Roman Nazarenko <me@jtalk.me>
2021-08-07 21:04:55 +0100

    More ignores

--- home2-ui/component/error/web-error.jsx
+++ home2-ui/component/error/web-error.jsx
@@ -28,12 +28,15 @@ const ignore404 = [
   /\.php(\?|$)/i,
   /asset-manifest.json$/i,
   /\/(\.env|feed|ads\.txt|wp-\w+|wp\d|rss\.xml|console|admin(istrator)?|vendor|bak|new-site|v\d|temp|\d{4}|dev|web|old(-\w+)?|cms|demo|backup)/i,
-  /\/(site|test|main|new|wordpress|bk|adm|ics|m8s|aws\.yml|phpinfo|_profiler)/i,
+  /\/(site|test|main|new|wordpress|bk|adm|ics|m8s|aws\.yml|phpinfo|_profiler|manager|uploads|user|files)/i,
   /\/(\.git|DesktopModules|providers|Install|app_master|\.well-known|stalker_portal|c|home\/resources)/i,
   /\/(\w+\/)?(login|sign.in)/i,
+  /\/javax\.faces\.resource\//i,
+  /\/images$/,
   /\/index\.rdf/i,
   /\/rss.xml/i,
   /\/\.well-known\/(acme-challenge\/|security.txt)/i,
+  /\/humans.txt/i,
 ];
 function is404Ignored(path) {
   for (const ignored of ignore404) {
