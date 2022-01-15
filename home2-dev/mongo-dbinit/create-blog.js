88c24f1f71f350eea45d3a90d7b14036c2037b34
Roman Nazarenko <me@jtalk.me>
2022-01-15 17:03:46 +0000

    Extend the number of test articles to aid Atom tests count check.

--- home2-dev/mongo-dbinit/create-blog.js
+++ home2-dev/mongo-dbinit/create-blog.js
@@ -1,12 +1,12 @@
 
 print('Creating collection Articles');
 
-function asIso(prefix, suffix, hour) {
-	let hourString = '' + (hour + 1);
-	if (hourString.length === 1) {
-		hourString = '0' + hourString;
+function asIso(prefix, suffix, minute) {
+	let minuteString = '' + (minute + 1);
+	if (minuteString.length === 1) {
+		minuteString = '0' + minuteString;
 	}
-	return prefix + hourString + suffix;
+	return prefix + minuteString + suffix;
 }
 
 function pad2(value) {
@@ -48,13 +48,13 @@ db.articles.insert({
 	tags: [],
 	atomId: "8f855752-23a6-4af5-b434-31b9aeda4b6c"
 });
-Array(20).fill().forEach((_, i) => {
+Array(40).fill().forEach((_, i) => {
 	db.articles.insert({
 		title: 'Test Article ' + (i + 3),
 		id: 'test-article-' + (i + 3),
 		published: i % 3 !== 0,
-		created: asIso('2020-01-20T', ':00:01Z', (i + 1)),
-		updated: asIso('2020-05-02T', ':00:01Z', (i + 1)),
+		created: asIso('2020-01-20T01:', ':01Z', (i + 1)),
+		updated: asIso('2020-05-02T01:', ':01Z', (i + 1)),
 		content: '<preview>A very cool test article</preview>',
 		tags: ["Java", "Scala"],
 		atomId: "c33f90f8-d3ca-480b-a71f-6ea3fda350" + pad2(i + 1)
