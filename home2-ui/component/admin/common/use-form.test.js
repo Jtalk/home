7b223917b8ff71a4d4c2344d914e9110664cd787
Roman Nazarenko <me@jtalk.me>
2022-08-20 19:03:54 +0100

    Add the new hooks testing library

--- home2-ui/component/admin/common/use-form.test.js
+++ home2-ui/component/admin/common/use-form.test.js
@@ -1,4 +1,4 @@
-import { act, renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react";
 import { reportError } from "../../../utils/error-reporting";
 import { useForm } from "./use-form";
 
